b26fb081be90bea5bad1d98b05bdda721a019b28
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-05-03 10:13:21 +0200

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,43 +1,46 @@
+/* eslint no-alert:off */
+
 // external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
+import messagesReducer from './reducers/messages_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
+
+const identityReducer = (state = null) => state;
+
 const initialState = {
-  messages: [
-    {
-      "author":"anonymous92",
-      "content":"Hello world!",
-      "created_at":"2017-09-26T23:03:16.365Z"
-    },
-    {
-      "author":"anonymous77",
-      "content":"My name is anonymous77",
-      "created_at":"2017-09-26T23:03:21.194Z"
-    }
-  ],
+  messages: [],
   channels: ['general', 'react', 'paris'],
-  selectedChannel: 'general',
-  currentUser: prompt("What is your username?"),
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
 };
 
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
-  // Add the identity reducer for every key in the state
+  messages: messagesReducer,
+  channels: identityReducer,
+  currentUser: identityReducer,
+  selectedChannel: selectedChannelReducer
 });
+
+// Middlewares
+const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
 
-export default initialState;
