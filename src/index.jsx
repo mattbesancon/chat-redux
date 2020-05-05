4ae01bc4b90c19e391b0f60b75856ee4852ffed9
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-05-05 21:32:53 +0200

    index

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 /* eslint no-alert:off */
 
-// external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -8,11 +7,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import reduxPromise from 'redux-promise';
 
-// internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
-// State and reducers
 import messagesReducer from './reducers/messages_reducer';
 import selectedChannelReducer from './reducers/selected_channel_reducer';
 
@@ -32,11 +29,9 @@ const reducers = combineReducers({
   selectedChannel: selectedChannelReducer
 });
 
-// Middlewares
 const middlewares = applyMiddleware(reduxPromise, logger);
 const store = createStore(reducers, initialState, middlewares);
 
-// render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={store}>
     <App />
